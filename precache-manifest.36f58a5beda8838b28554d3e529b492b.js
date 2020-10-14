3598c90008c53e2e107e22801998597d56ff7977
nicolecpineda <nicolecpineda@gmail.com>
2020-10-14 00:58:25 -0400

    Updates

---
+++ precache-manifest.36f58a5beda8838b28554d3e529b492b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2283e6cd557894454fa98c488dd2820",
+    "url": "/cooking-calculator/index.html"
+  },
+  {
+    "revision": "ebf761ffdf25226052d1",
+    "url": "/cooking-calculator/static/css/main.6741f3c0.chunk.css"
+  },
+  {
+    "revision": "14637a93c7bfb50fd9e1",
+    "url": "/cooking-calculator/static/js/2.83ddee46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cooking-calculator/static/js/2.83ddee46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebf761ffdf25226052d1",
+    "url": "/cooking-calculator/static/js/main.a365929d.chunk.js"
+  },
+  {
+    "revision": "160b986ae98d07d8123a",
+    "url": "/cooking-calculator/static/js/runtime-main.6bf2e85a.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/cooking-calculator/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
